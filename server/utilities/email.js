b686101a4631261cc8e9376d2aaecc81d1dfbfba
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-11-09 16:11:57 +0100

    Commit before restructuring api

---
+++ server/utilities/email.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+dotenv.config();
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.OAUTH_CLIENT_ID,
+  process.env.OAUTH_CLIENT_SECRET,
+  process.env.OAUTH_REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN });
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.APP_EMAIL,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: await oAuth2Client.getAccessToken(),
+  },
+});
+
+const sendEmail = (fromName, fromEmail, toName, toEmail, subjectLine, message) {
+  
+};
